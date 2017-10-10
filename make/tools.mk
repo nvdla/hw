b70af4d43b01ac64ea294f0d26384bf5a8a44a18
Eric Zhang <ezhang@nvidia.com>
2017-10-09 17:41:01 -0700

    add vcp

--- make/tools.mk
+++ make/tools.mk
@@ -1,8 +1,10 @@
 # c pre-processor
-CPP  := /home/gnu/bin/cpp
+CPP  := /home/utils/gcc-4.0.0/bin/cpp
 
 # sed
-SED  := /bin/sed
+SED  ?= /bin/sed
 
 # used in ordt to generate reigster, need be newer than 1.7
-JAVA := /home/utils/java/jdk1.8.0_131/bin/java
+JAVA ?= /home/utils/java/jdk1.8.0_131/bin/java
+
+VCP := $(DEPTH)/bin/vcp
