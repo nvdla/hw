30aa539867e97470bf231b05f8cf35627c6859db
Eric Zhang <ezhang@nvidia.com>
2017-10-11 03:21:46 -0700

    vcp with eperl

--- make/tools.mk
+++ make/tools.mk
@@ -7,4 +7,8 @@ SED  ?= /bin/sed
 # used in ordt to generate reigster, need be newer than 1.7
 JAVA ?= /home/utils/java/jdk1.8.0_131/bin/java
 
+# pre-process for ifdef/define
 VCP := $(DEPTH)/bin/vcp
+
+# embedded perl 
+EPERL := $(DEPTH)/bin/eperl
