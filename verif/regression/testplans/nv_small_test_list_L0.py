b990a1890b03a81b7a970c6c3a31fd8ba1ae7299
Peter Li <penli@nvidia.com>
2018-06-05 00:08:54 -0700

    Update test plan, make register accessing test as rtl_only

--- verif/regression/testplans/nv_small_test_list_L0.py
+++ verif/regression/testplans/nv_small_test_list_L0.py
@@ -10,7 +10,7 @@ add_test(name='nvdla_reg_accessing',
 add_test(name='nvdla_reg_accessing',
          tags=['L0'],
          module='nvdla_python_test',
-         args=[FIXED_SEED_ARG, DISABLE_COMPARE_ALL_UNITS_SB_ARG],
+         args=[FIXED_SEED_ARG, DISABLE_COMPARE_ALL_UNITS_SB_ARG, '-uwm RTL_ONLY'],
          config=['nvdla_utb'],
          desc='''Check reset value''')
 
