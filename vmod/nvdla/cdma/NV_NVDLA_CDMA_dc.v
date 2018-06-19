2becf69937b7409d21805aa4fda22659cd1204f0
tonyz <tonyz@nvidia.com>
2018-06-18 21:45:34 -0700

    fix rtl for spyglass issue and add waive file for cdma

--- vmod/nvdla/cdma/NV_NVDLA_CDMA_dc.v
+++ vmod/nvdla/cdma/NV_NVDLA_CDMA_dc.v
@@ -799,10 +799,18 @@ assign slcg_dc_gate_img = slcg_dc_gate_d3[1];
 ////////////////////////////////////////////////////////////////////////
 //: my $atmm = NVDLA_MEMORY_ATOMIC_SIZE;
 //: my $atmbw = int(log(${atmm})/log(2));
+//: if($atmbw > 3){
 //: print qq(
 //:     assign data_width_sub_one_w = (is_packed_1x1) ? {{(2+${atmbw}){1'b0}}, reg2dp_datain_channel[12:${atmbw}]} : {2'b0, reg2dp_datain_width};
 //:     assign data_surface_inc = {{(${atmbw}-3){1'b0}}, reg2dp_datain_channel[12:${atmbw}]} + 1'b1;
 //: );
+//:}
+//: else {
+//: print qq(
+//:     assign data_width_sub_one_w = (is_packed_1x1) ? {{(2+${atmbw}){1'b0}}, reg2dp_datain_channel[12:${atmbw}]} : {2'b0, reg2dp_datain_width};
+//:     assign data_surface_inc = {reg2dp_datain_channel[12:${atmbw}]} + 1'b1;
+//: );
+//:}
 
 // assign is_data_expand = 1'b0;
 //assign is_data_shrink = 1'b0;
