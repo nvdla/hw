51b42dbb3a2590b922042b55f9096686c40136d6
Yilin Zhang <yilinz@nvidia.com>
2018-08-13 02:31:02 -0700

    shrinked the size of sdp test cases

--- verif/regression/testplans/nv_large_test_list_L0.py
+++ verif/regression/testplans/nv_large_test_list_L0.py
@@ -14,11 +14,11 @@ add_test(name='pdp_8x8x32_1x1_max_int8_0',
          desc='''copied from pdp_passthrough_8x8x32_pack_inc_int8''')
 
 ############################################# SDP ###################################
-#add_test(name='sdp_13x4x29_ew_lut_int8',    #sdp_cmod_full_feature_7
-#         tags=['L0', 'sdp'],
-#         args=[FIXED_SEED_ARG, DISABLE_COMPARE_ALL_UNITS_SB_ARG],
-#         config=['nvdla_utb'],
-#         desc='''copied from sdp_cmod_full_feature_7, input: 13x4x29, BS/BN disabled, EW enabled w/ MUL(per-channel),LUT enabled, both input/output are INT8''')
+add_test(name='sdp_13x4x29_ew_lut_int8',    #sdp_cmod_full_feature_7
+         tags=['L0', 'sdp'],
+         args=[FIXED_SEED_ARG, DISABLE_COMPARE_ALL_UNITS_SB_ARG],
+         config=['nvdla_utb'],
+         desc='''copied from sdp_cmod_full_feature_7, input: 13x4x29, BS/BN disabled, EW enabled w/ MUL(per-channel),LUT enabled, both input/output are INT8''')
 
 ############################################# CDP ###################################
 add_test(name='cdp_8x8x64_lrn9_int8_0',
